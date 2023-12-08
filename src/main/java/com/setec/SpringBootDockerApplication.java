27c475b1f5bef6ea9e5d020bffc312ac9fd7f6b1
Sum Chanthou <chanthousum2017@gmail.com>
2023-12-08 18:17:30 +0700

    new project

---
+++ src/main/java/com/setec/SpringBootDockerApplication.java
@@ -0,0 +1,24 @@
+package com.setec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class SpringBootDockerApplication {
+	@GetMapping("/message")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("/save")
+	public String save() {
+		return "save";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
